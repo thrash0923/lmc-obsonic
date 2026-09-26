f9566d62b374d00eb90981cc9dd1f82064b7f4ee
thrash0923 <thrash0923@gmail.com>
2026-09-26 18:13:26 +0900

    Add analytics loader

---
+++ js/analytics-loader.js
@@ -0,0 +1,8 @@
+/* LMC OB-SONIC analytics loader */
+(function () {
+  "use strict";
+  var script = document.createElement("script");
+  script.src = "/js/ga4.js?v=20260926";
+  script.async = true;
+  document.head.appendChild(script);
+})();
