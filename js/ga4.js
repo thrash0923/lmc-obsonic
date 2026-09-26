b09f224a0fd1a1273213ce274f8b423f524b6583
thrash0923 <thrash0923@gmail.com>
2026-09-26 18:13:20 +0900

    Add GA4 tracking script

---
+++ js/ga4.js
@@ -0,0 +1,16 @@
+/* LMC OB-SONIC Google Analytics 4 */
+(function () {
+  "use strict";
+
+  const measurementId = "G-DQQYSYDYGH";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId);
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(measurementId);
+  document.head.appendChild(script);
+})();
